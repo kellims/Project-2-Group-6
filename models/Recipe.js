767c3171b78b0025e0a7585b9136555c6a7074a6
mustafacev <mustafaorcuncevik@gmail.com>
2023-04-11 19:12:28 -0400

    setting up the models files

---
+++ models/Recipe.js
@@ -0,0 +1,48 @@
+module.exports = mongoose.model('Recipe', recipeSchema)
+const recipeSchema = new mongoose.Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        unique: true,
+        required: [true, 'Recipe name is required!'],
+    },
+    ingredients: {
+        type: Array, // array feature 
+        required: true
+    },
+    nutrients: {
+        type: Array, // array feature 
+        required: true
+    },
+    cookingTime: {
+        type: String, //number feature nice way
+        trim: true,
+    },
+    instructions: {
+        type: String,
+        required: true
+    },
+    measurement: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+       // default: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/1280px-Good_Food_Display_-_NCI_Visuals_Online.jpg', // edit link 
+    },
+    category: {
+        type: String,
+        required: true,
+      //  enum: ['American', 'Turkishcusine', 'French', 'Desserts']    // I need to edit this section.
+    },
+    authorUser: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+const Recipe = mongoose.model('Recipes', recipeSchema);
+module.exports = Recipe;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
-// This connects the config file that links MongoDB and Mongoose to the rules for each of the dbs we create
+
 require('../configuration/connection');
 
 module.exports = {
-    recipes: require('./Recipes')
+    recipes: require('./Recipe'),
+    review : require('./Review'),
+    users : require('./User'),
+    
 }
